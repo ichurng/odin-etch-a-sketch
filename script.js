2b1cc64acfb68b4f7137f505992c0d9ab7d16c78
I Chun Hurng <ichunhurng@gmail.com>
2022-06-13 20:24:55 -0700

    Add hover effect. Remove grid borders.

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 
 
 
-let container = document.createElement('div');
-container.classList.add('container');
-document.body.appendChild(container);
+let container = document.querySelector('.container');
 
 // Creating 16x16 div within a container div
 let parentDiv;
 let childDiv;
+let counter = 0;
 for (let i = 0; i < 16; i ++){
     parentDiv = document.createElement('div');
     parentDiv.classList.add("parent-div");
     for (let j = 0; j < 16; j ++){
         childDiv = document.createElement('div');
+        childDiv.setAttribute('id', `'${counter}'`);
         childDiv.classList.add('child-div');
         parentDiv.appendChild(childDiv);
+        counter += 1;
     }
     container.appendChild(parentDiv);
 }
 
+function etch(e){
+    let cell = e.target;
+    cell.classList.add('hover');
+}
+
+let squares = document.querySelectorAll('.child-div');
+squares.forEach((square) => square.addEventListener('mouseenter', etch));
 
 
